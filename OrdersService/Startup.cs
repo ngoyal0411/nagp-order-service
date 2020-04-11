4760f6e1bea0a048ebf0dfd7ce35b82155904da5
ngoyal0411 <ngoyal0411@gmail.com>
2020-04-12 00:42:51 +0530

    Jaeger service implementation - distributing tracing

--- OrdersService/Startup.cs
+++ OrdersService/Startup.cs
@@ -1,7 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
 using System.Threading.Tasks;
+using Jaeger;
+using Jaeger.Reporters;
+using Jaeger.Samplers;
+using Jaeger.Senders;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
@@ -10,6 +15,8 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
+using OpenTracing;
+using OpenTracing.Util;
 using OrdersService.Repository;
 
 namespace OrdersService
@@ -30,6 +37,33 @@ namespace OrdersService
 
             // configure DI for application services
             services.AddScoped<IOrderBusiness, OrderBusinessLogic>();
+
+            services.AddSingleton<ITracer>(serviceProvider =>
+            {
+                string serviceName = Assembly.GetEntryAssembly().GetName().Name;
+
+                ILoggerFactory loggerFactory = serviceProvider.GetRequiredService<ILoggerFactory>();
+
+                ISampler sampler = new ConstSampler(sample: true);
+
+                var reporter = new RemoteReporter.Builder()
+                        .WithLoggerFactory(loggerFactory)
+                        .WithSender(new UdpSender("jaeger-agent", 6831, 0))
+                        .Build();
+
+                ITracer tracer = new Tracer.Builder(serviceName)
+                    .WithLoggerFactory(loggerFactory)
+                    .WithSampler(sampler)
+                    .WithReporter(reporter)
+                    .Build();
+
+                GlobalTracer.Register(tracer);
+
+                return tracer;
+            });
+
+            services.AddOpenTracing();
+
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
